2fe1f60880139bb3d40b0e2925d91831535ca8ed
MarouaHattab <maroua.hattab@polytechnicien.tn>
2025-11-27 16:43:59 +0100

    fix postgress connection

--- src/main.py
+++ src/main.py
@@ -5,27 +5,27 @@ from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
 from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 from stores.llm.templates.template_parser import TemplateParser
-from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 
 async def startup_span():
     settings = get_settings()
-    postgress_con= f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRESS_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
+    postgress_con = f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
     app.db_engine = create_async_engine(postgress_con)
     app.db_client = sessionmaker(
         app.db_engine, 
-        class_=AsyncSession, # session must be async 
-        expire_on_commit=False # to prevent session from expiring objects after commit
-        )
+        class_=AsyncSession,  # session must be async 
+        expire_on_commit=False  # to prevent session from expiring objects after commit
+    )
 
     llm_provider_factory = LLMProviderFactory(settings)
     vectordb_provider_factory = VectorDBProviderFactory(settings)
 
     # generation client
     app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
-    app.generation_client.set_generation_model(model_id = settings.GENERATION_MODEL_ID)
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
 
     # embedding client
     app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
@@ -47,10 +47,12 @@ async def startup_span():
 async def shutdown_span():
     app.db_engine.dispose()
     app.vectordb_client.disconnect()
-   
-    app.on_event("startup")(startup_span)
-    app.on_event("shutdown")(shutdown_span)
 
-    app.include_router(base.base_router)
-    app.include_router(data.data_router)
-    app.include_router(nlp.nlp_router)
+# Register event handlers
+app.on_event("startup")(startup_span)
+app.on_event("shutdown")(shutdown_span)
+
+# Include routers
+app.include_router(base.base_router)
+app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
