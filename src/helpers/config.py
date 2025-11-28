2b6bee2c1b065c721a619c8dca118e079bd753d6
MarouaHattab <maroua.hattab@polytechnicien.tn>
2025-11-29 00:36:00 +0100

    update add vector db index threshold

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -34,6 +34,7 @@ class Settings(BaseSettings):
     VECTOR_DB_BACKEND : str
     VECTOR_DB_PATH : str
     VECTOR_DB_DISTANCE_METHOD: str = None
+    VECTOR_DB_PGVEC_INDEX_THRESHOLD : int
 
     PRIMARY_LANG: str = "en"
     DEFAULT_LANG: str = "en"
