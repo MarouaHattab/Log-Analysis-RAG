3c36e5543886c83f9f8301980f1efbe7a57c35e2
MarouaHattab <maroua.hattab@polytechnicien.tn>
2025-12-23 03:55:53 +0100

    fix problem

--- docker/minirag/entrypoint.sh
+++ docker/minirag/entrypoint.sh
@@ -2,16 +2,8 @@
 set -e
 
 echo "Running database migrations..."
-cd /app/models/db_schemes/minirag
-
-# Try to upgrade. If it fails due to existing tables, stamp to head.
-# The || handles the failure case
-alembic upgrade head || {
-    echo "Migration failed, tables may already exist. Stamping to head..."
-    alembic stamp head
-    echo "Stamped successfully."
-}
-
+cd /app/models/db_schemes/minirag/
+alembic upgrade head
 cd /app
 
 # Execute the CMD passed from Dockerfile (uvicorn)
